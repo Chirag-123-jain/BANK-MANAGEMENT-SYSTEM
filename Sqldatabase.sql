a09ff9f9dbdb638cbfacad8e409c18147407053c
Chirag-123-jain <77845590+Chirag-123-jain@users.noreply.github.com>
2022-06-05 15:49:03 +0530

    Add files via upload

---
+++ Sqldatabase.sql
@@ -0,0 +1,7 @@
+create database bankmanagementsystem;
+
+create table Signup(formno varchar(20),name varchar(20),fname varchar(20),adress varchar(40),state varchar(20),city varchar(20),dob varchar(20),gender varchar(20),marital_status varchar(20),pin varchar(20));
+ 
+show tables;
+select *from Signup;
+ create table Signuotwo(formno varchar(20),sreligion varchar(20),scategory varchar(20),sincome varchar(20),seducation varchar(20),soccupation varchar(20),seniorcitizen varchar(20),existingaccount varchar(20),span varchar(20),saadhar varchar(20))
